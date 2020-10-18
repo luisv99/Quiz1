3cbb1ac7f6dad236e07d98d093e58df64041bc59
Luis Vargas <luisito_v99@hotmail.com>
2020-10-18 16:17:22 -0400

    array js

--- main.js
+++ main.js
@@ -6,3 +6,25 @@ const alerta = () => {
 const btn = document.getElementById('btn')
 
 btn.onclick = alerta;
+
+
+skills = [
+        
+        {label: 'html',
+        data: 60},
+        
+        {label: 'css',
+        data: 65},
+        
+        {label: 'python',
+        data: 70},
+        
+        {label: 'javascript',
+        data: 10}
+]
+const skill = document.getElementById('skills')
+
+for (let index = 0; index < skills.length; index++) {
+        const element = skills[index];
+        console.log(element)
+}
